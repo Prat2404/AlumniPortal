362995bd402597989bcd37829dc2de50fc5a5f3d
abhikeshri10 <60318491+abhikeshri10@users.noreply.github.com>
2021-05-29 01:55:58 +0530

    Login Signup Base Template update #01

--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('login/',views.login,name='user-login'),
     path('signup/',views.signup,name='user-signup'),
 ]
--- user/views.py
+++ user/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def signup(request):
     return render(request,'user/signup.html')
+
+def login(request):
+    return render(request,'user/login.html')
